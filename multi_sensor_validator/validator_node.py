13ca057bb77c5447602beda0902671b37f8fefe0
Samer <samer.mohamed600012@gmail.com>
2025-09-22 16:07:47 +0300

    add some comments

--- multi_sensor_validator/validator_node.py
+++ multi_sensor_validator/validator_node.py
@@ -9,12 +9,15 @@ class ValidatorNode(Node):
         self.get_logger().info("Initializing validator node")
         #self.ultrasonic_sub = self.create_subscription(Int32,"/ultrasonic_range",self.ultrasonic_listener,10)
         #self.infrared_sub = self.create_subscription(Int32,"/infrared_range",self.infrared_listener,10)
-        self.ultrasonic_sub = Subscriber(self,Int32,"/ultrasonic_range",qos_profile=10)
+
+        #Use message_filters to sync the two topics
+        self.ultrasonic_sub = Subscriber(self,Int32,"/ultrasonic_range",qos_profile=10) 
         self.infrared_sub = Subscriber(self,Int32,"/infrared_range",qos_profile=10)
         self.validation_pub = self.create_publisher(String,"/validation_result",10)
         self.synced_sub = ApproximateTimeSynchronizer([self.ultrasonic_sub,self.infrared_sub],10,slop=.1,allow_headerless=True) 
         self.synced_sub.registerCallback(self.validate)
 
+    #check if readings are consistent
     def validate(self,msg1:Int32,msg2:Int32):
         print(f"ultrasonic : {msg1.data} cm | infrared : {msg2.data} cm | difference : {abs(msg1.data - msg2.data)}")
         msg = String()
@@ -23,6 +26,7 @@ class ValidatorNode(Node):
         else:
             msg.data = "Sensor readings inconsistent"
         self.validation_pub.publish(msg)
+
 def main(args = None):
     rclpy.init(args=args)
     node = ValidatorNode()
